2fd8889f45e0e78983d5343ad8d262b55ef78228
badjoke-lab <badjoke.lab@gmail.com>
2026-06-03 16:09:17 +0900

    PR-10: add robots endpoint

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,9 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = () => {
+  return new Response(`User-agent: *\nAllow: /\n\nSitemap: https://cya.badjoke-lab.com/sitemap.xml\n`, {
+    headers: {
+      'Content-Type': 'text/plain; charset=utf-8',
+    },
+  });
+};
