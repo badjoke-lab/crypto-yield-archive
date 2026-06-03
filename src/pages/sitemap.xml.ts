c8f1c7b7eb4e5325f641667fc5d42b708e3a48b3
badjoke-lab <badjoke.lab@gmail.com>
2026-06-03 16:11:46 +0900

    PR-10: add sitemap endpoint

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,18 @@
+import type { APIRoute } from 'astro';
+import { allPlatforms } from '../lib/data';
+
+const SITE = 'https://cya.badjoke-lab.com';
+
+export const GET: APIRoute = () => {
+  const staticPaths = ['', 'outcomes/', 'bankruptcy-cases/', 'methodology/', 'about/'];
+  const platformPaths = allPlatforms.map((platform) => `platform/${platform.slug}/`);
+  const urls = [...staticPaths, ...platformPaths]
+    .map((path) => `  <url><loc>${SITE}/${path}</loc></url>`)
+    .join('\n');
+
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${urls}\n</urlset>\n`, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+    },
+  });
+};
